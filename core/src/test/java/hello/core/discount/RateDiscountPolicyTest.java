e656b8eaad15c7d5af010954d19d3ff11e0b02af
Lee, HuiJu <leemoon63@gmail.com>
2023-04-03 23:23:48 +0900

    feat: apply RateDiscountPolicy

--- core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -1,6 +1,5 @@
 package hello.core.discount;
 
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.junit.jupiter.api.DisplayName;
