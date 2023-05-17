77703f27991d4b82be8c15c050f5f113aa5b90cc
Lee, HuiJu <leemoon63@gmail.com>
2023-05-17 15:08:47 +0900

    simple-webservice project 최초 commit

---
+++ simple-webservice/src/test/java/simple/simplewebservice/SimpleWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package simple.simplewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
