1aa2e339ceb6d4daa71f42390b9a9646b3e1b5e8
Lee, HuiJu <leemoon63@gmail.com>
2023-05-31 15:53:58 +0900

    test: test and fix the indexPage error

---
+++ simple-webservice/src/test/java/simple/simplewebservice/web/IndexControllerTest.java
@@ -0,0 +1,23 @@
+package simple.simplewebservice.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        Assertions.assertThat(body).contains("simple-webservice");
+    }
+}
