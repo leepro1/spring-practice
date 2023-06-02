bfc0bf708562d6493c5e53a0f7dfbf7425eae5bf
Lee, HuiJu <leemoon63@gmail.com>
2023-06-02 21:57:53 +0900

    feat: add posts delete UI

--- simple-webservice/src/main/resources/static/js/app/index.js
+++ simple-webservice/src/main/resources/static/js/app/index.js
@@ -3,7 +3,13 @@ const main = {
         const _this = this;
         $('#btn-save').on('click', function() {
             _this.save();
-        })
+        });
+        $('#btn-update').on('click', function() {
+            _this.update();
+        });
+        $('#btn-delete').on('click', function() {
+            _this.delete();
+        });
     },
     save : function() {
         const data = {
@@ -36,12 +42,28 @@ const main = {
 
         $.ajax({
             type: "PUT",
-            url: '/api/v1/posts'+id,
+            url: '/api/v1/posts/'+id,
             dataType: 'json',
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        });
+    },
+
+    delete : function() {
+        const id = $('#id').val();
+
+        $.ajax({
+            type: "DELETE",
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
             window.location.href = '/';
         }).fail(function(error) {
             alert(JSON.stringify(error));
